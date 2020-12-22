93f564bccab7db92eddd604fc56cdcd2240286e9
Erbeibei <623720661@qq.com>
2020-12-22 18:08:02 +0800

    记录 8

--- test/page/zy_school/home_page.py
+++ test/page/zy_school/home_page.py
@@ -6,17 +6,18 @@ import sys
 sys.setrecursionlimit(100000)
 """
 首页元素定位
-url:http://www.zhongye.net/###
+url:http://api.iscportal.isc.glb.cmos:50002/iscportal/index.html#/
 封装登录页所有页面元素用于case统一调用
 """
 
 
 class HomePage(Page, Browser):
     #   元素定位及实现
-    username_loc = (By.ID, 'txtname')
-    password_loc = (By.ID, 'txtpass')
-    loginBtn_loc = (By.ID, 'loginBtn')
-    registerBtn_loc = (By.CLASS_NAME, 'regist')
+    username_loc = (By.CLASS_NAME, 'login_ipt_user')
+    password_loc = (By.CLASS_NAME, 'txtpass')
+    sendCode_loc = (By.CLASS_NAME, 'send-code')
+    inputCode_loc = (By.CLASS_NAME, 'item-inp-lo phone-code')
+    loginBtn_loc = (By.CLASS_NAME, 'btn-box')
 
     ##登录账号
     def input_username(self, username):
@@ -28,12 +29,19 @@ class HomePage(Page, Browser):
         logger.info("输入密码：%s", password)
         self.find_element(*self.password_loc).send_keys(password)
 
+    ##获取验证码按钮
+    def sendCode_clike(self):
+        logger.info("点击获取验证码按钮")
+        self.find_element(*self.sendCode_loc).click()
+
+    ##验证码录入，测试环境默认123456
+    def input_sendCode(self):
+        logger.info("验证码录入")
+        self.find_element(*self.inputCode_loc).send_keys('123456')
+
     ##登录按钮
     def loginBtn_click(self):
         logger.info("登录按钮点击")
         self.find_element(*self.loginBtn_loc).click()
 
-    ##免费注册按钮
-    def registerBtn_click(self):
-        logger.info("免费注册按钮点击")
-        self.find_element(*self.registerBtn_loc).click()
+
--- utils/file_reader.py
+++ utils/file_reader.py
@@ -80,10 +80,10 @@ class ExcelReader:
 
 
 if __name__ == '__main__':
-    y = 'E:\Test_framework\config\config.yml'
+    y = '/Users/apple/PycharmProjects/UIAutoCode/config/config.yml'
     reader = YamlReader(y)
     print(reader.data)
 
-    e = 'E:/Test_framework/data/baidu.xlsx'
-    reader = ExcelReader(e, title_line=True)
-    print(reader.data)
+    # e = 'E:/Test_framework/data/baidu.xlsx'
+    # reader = ExcelReader(e, title_line=True)
+    # print(reader.data)
